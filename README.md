8967582c5313d02454aff336d83b25d57d690513
potatoland4492 <potatoland4492@gmail.com>
2022-01-15 11:03:43 -0800

    Update README.md

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # TurtleLib
+
+TurtleLib is a library of scripts that interact with the PythonTurtle module to create games, drawings, and more!
+
+## Installing PythonTurtle
+
+### Debian
+
+```BASH
+$ sudo apt-get install --no-install-recommends python3-wxgtk4.0
+$ sudo apt-get install --no-install-recommends python3-tk
+$ sudo apt-get install --no-install-recommends python3-pip
+$ python3 -m pip install --user PythonTurtle
+```
+
+### Other
+
+See [PythonTurtle's git repository on Github](https://github.com/PythonTurtle/PythonTurtle).
+
+## Scripts
+
+### Race
+
+What this script does: It sets up a simple race layout for two players, one green and the other blue.
+How to use: Copy the `race.py` script into your current working directory. Start Python and run `import race`. When the game is set up, the script will prompt you to start the game. Press `Enter` on your keyboard to begin.
---
+++ scripts/race.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+import time
+import random
+import turtle
+s = turtle.getscreen()
+t = turtle
+t.shapesize(2.5)
+t.shape('turtle')
+t.pensize(5)
+t.pu()
+t.goto(-500, 200)
+t.pd()
+t.fd(1000)
+t.pu()
+t.lt(90)
+t.fd(50)
+t.pd()
+t.bk(100)
+t.pu()
+t.goto(-500, -200)
+t.rt(90)
+t.pd()
+t.fd(1000)
+t.pu()
+t.lt(90)
+t.fd(50)
+t.pd()
+t.bk(100)
+t.pu()
+t.home()
+g = turtle.Turtle()
+g.shapesize(2.5)
+g.shape('turtle')
+g.pensize(5)
+g.color('green')
+g.pu()
+g.goto(-500, 200)
+g.pd()
+b = turtle.Turtle()
+b.shapesize(2.5)
+b.shape('turtle')
+b.pensize(5)
+b.color('blue')
+b.pu()
+b.goto(-500, -200)
+b.pd()
+
+input("Setup complete. Press `Enter` to start the game.")
+
+while g.xcor() < 500 and b.xcor() < 500:
+  time.sleep(1)
+  g.fd(random.randint(10, 100))
+  time.sleep(1)
+  b.fd(random.randint(10, 100))
+if g.xcor() >= 500 and b.xcor() >= 500:
+  print("Both players tied!")
+elif g.xcor() >= 500 and b.xcor() < 500:
+  print("The green player won!")
+elif b.xcor() >= 500 and g.xcor() < 500:
+  print("The blue player won!")
